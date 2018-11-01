714a531e1485460c42c696bea94b921d74dd8163
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-11-01 12:46:54 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
+
   def index
     @students = Student.all
   end
+
+  def show # show action
+    @student = Student.find(params[:id])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
   resources :students, only: :index
+
+  get 'students/:id', to: "students#show"
+  # can be written using resources method like this:
+  # resources :students, only: [:index, :show]
+
+
 end
